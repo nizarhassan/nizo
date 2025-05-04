64329fadbebdb73fc48f5aeddfdf205b35ccfc68
nizarhassan <abudanya2023@gmail.com>
2025-05-04 11:24:07 +0300

    init pro

---
+++ lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+import 'package:quran/homescreen.dart';
+
+void main(){
+  runApp(Myapp());
+}
+ class Myapp extends StatelessWidget {
+   const Myapp({super.key});
+
+   @override
+   Widget build(BuildContext context) {
+     return MaterialApp(
+       initialRoute:Homescreen.routName ,
+       routes: {
+         Homescreen.routName:(context)=>Homescreen()
+       },
+     );
+   }
+ }
+
